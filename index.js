480038b6b2119b7cf636e990872982950263e1a1
2salted <noamgraf123@gmail.com>
2024-09-12 19:28:05 +0000

    added image slider

---
+++ index.js
@@ -0,0 +1,39 @@
+const modal = document.getElementById("modal");
+const outOfSlider = document.getElementsByClassName("slider-container");
+
+document.querySelectorAll('.btn-test').forEach(button => {
+  button.addEventListener('click', () => {
+    modal.style.display = 'flex';
+  });
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slider-img');
+//
+// function moveSlide(direction) {
+//   slides.forEach((slide, index) => {
+//     slide.classList.remove('left-img', 'center-img', 'right-img');
+//   });
+//
+//   currentSlide += direction;
+//
+//   if (currentSlide < 0) {
+//     currentSlide = slides.length - 1;
+//   } else if (currentSlide >= slides.length) {
+//     currentSlide = 0;
+//   }
+//
+//   let nextSlide = (currentSlide + 1) % slides.length;
+//   let prevSlide = (currentSlide - 1 + slides.length) % slides.length;
+//
+//   slides[prevSlide].classList.add('left-img');
+//   slides[currentSlide].classList.add('center-img');
+//   slides[nextSlide].classList.add('right-img');
+// }
+
