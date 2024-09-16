3c7b1911fa89eb02735e0048da3909bb4ba1a7bb
2salted <noamgraf123@gmail.com>
2024-09-16 22:18:23 +0000

    committing before adding tailwind to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,60 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import whatsapp from './images/whatsapp.png';
 
 export default function App() {
+
+  const [btnImage, setBtnImage] = useState(null);
+
   return (
     <div>
       <div id="headImage" alt="Miradouro de Faja Do Conde">
-        <div class="btn-test btn1">P</div>
-        <div class="btn-test btn2">P</div>
-        <div class="btn-test btn3">P</div>
-        <div class="btn-test btn4">P</div>
-        <div class="btn-test btn5">P</div>
-        <div class="btn-test btn6">P</div>
-        <div class="btn-test btn7">P</div>
-        <div class="btn-test btn8">P</div>
-        <div class="btn-test btn9">T</div>
-        <div class="btn-test btn10">P</div>
-        <div class="btn-test btn11">P</div>
-        <div class="btn-test btn12">T</div>
-        <div class="btn-test btn13">P</div>
+        <div className="btn-test btn1">P</div>
+        <div className="btn-test btn2">P</div>
+        <div className="btn-test btn3">P</div>
+        <div className="btn-test btn4">P</div>
+        <div className="btn-test btn5">P</div>
+        <div className="btn-test btn6">P</div>
+        <div className="btn-test btn7">P</div>
+        <div className="btn-test btn8">P</div>
+        <div className="btn-test btn9">T</div>
+        <div className="btn-test btn10">P</div>
+        <div className="btn-test btn11">P</div>
+        <div className="btn-test btn12">T</div>
+        <div className="btn-test btn13">P</div>
       </div>
-      <div class="sticky">
-        <a class="WhatsApp" aria-label="Chat on WhatsApp" href="https://api.whatsapp.com/send?phone=33624553500&text=">
-          <img alt="Chat on WhatsApp" src="images/whatsapp.png" /></a>
+      <div className="sticky">
+        <a className="WhatsApp" aria-label="Chat on WhatsApp" href="https://api.whatsapp.com/send?phone=33624553500&text=">
+          <img alt="Chat on WhatsApp" src={whatsapp} /></a>
       </div>
-      <div id="modal" class="modal">
-        <span class="arrow left-arrow" onclick="moveSlide(-1)">&#9664;</span>
+      <div id="modal" className="modal">
+        <span className="arrow left-arrow">&#9664;</span>
         <div class="slider">
-          <img src="images/testImage.jpg" class="sliderImage1 sideImages" />
-          <img src="images/testImage.jpg" class="sliderImage2 centerImage" />
-          <img src="images/testImage.jpg" class="sliderImage3 sideImages" />
         </div>
-        <span class="arrow right-arrow" onclick="moveSlide(1)">&#9654;</span>
+        <span className="arrow right-arrow">&#9654;</span>
       </div>
     </div>
   );
 }
-
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * */
