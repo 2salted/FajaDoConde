525f34b2e9b7acb0c75c75a85783787c28c24083
2salted <noamgraf123@gmail.com>
2024-09-16 21:25:59 +0000

    updated some items

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div id="headImage" alt="Miradouro de Faja Do Conde">
+        <div class="btn-test btn1">P</div>
+        <div class="btn-test btn2">P</div>
+        <div class="btn-test btn3">P</div>
+        <div class="btn-test btn4">P</div>
+        <div class="btn-test btn5">P</div>
+        <div class="btn-test btn6">P</div>
+        <div class="btn-test btn7">P</div>
+        <div class="btn-test btn8">P</div>
+        <div class="btn-test btn9">T</div>
+        <div class="btn-test btn10">P</div>
+        <div class="btn-test btn11">P</div>
+        <div class="btn-test btn12">T</div>
+        <div class="btn-test btn13">P</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div class="sticky">
+        <a class="WhatsApp" aria-label="Chat on WhatsApp" href="https://api.whatsapp.com/send?phone=33624553500&text=">
+          <img alt="Chat on WhatsApp" src="images/whatsapp.png" /></a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div id="modal" class="modal">
+        <span class="arrow left-arrow" onclick="moveSlide(-1)">&#9664;</span>
+        <div class="slider">
+          <img src="images/testImage.jpg" class="sliderImage1 sideImages" />
+          <img src="images/testImage.jpg" class="sliderImage2 centerImage" />
+          <img src="images/testImage.jpg" class="sliderImage3 sideImages" />
+        </div>
+        <span class="arrow right-arrow" onclick="moveSlide(1)">&#9654;</span>
+      </div>
+    </div>
+  );
 }
 
-export default App
