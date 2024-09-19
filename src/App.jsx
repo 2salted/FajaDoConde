7e1ef0bc44f1327064f26fd7fb0cc3ca86e9a8bf
2salted <noamgraf123@gmail.com>
2024-09-19 23:02:19 +0000

    added working sliding buttons

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ import galleryMedia from './galleryObjects.js'
 export default function App() {
   const [btnImage, setBtnImage] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [currImageIndex, setCurrImageIndex] = useState(0);
   let gallery;
 
   const searchGallery = (key) => {
-    return galleryMedia.find((item) => item.key == key)
+    return galleryMedia.find((item) => item.key == key);
   }
 
   if (showModal && btnImage !== null) {
@@ -57,33 +58,34 @@ export default function App() {
           }}
         >
           <span onClick={() => setShowModal(false)} className="sm:right-20 absolute top-12 right-10 text-white text-4xl font-bold hover:text-red-500 hover:cursor-pointer focus:outline-none">&times;</span>
-          <span className="arrow left-arrow">&larr;</span>
+          <span className="arrow left-arrow" onClick={() => {
+            if (currImageIndex > 0) {
+              setCurrImageIndex(currImageIndex - 1);
+            }
+          }}>&larr;</span>
           <div className="slider">
-            {/* <img src={gallery.gallery[0][0]} /> */}
+            <div>
+              <img src={gallery.gallery[currImageIndex]} width={100} height={100} />
+              <img src={gallery.gallery[currImageIndex + 1]} width={100} height={100} />
+              <img src={gallery.gallery[currImageIndex + 2]} width={100} height={100} />
+            </div>
           </div>
-          <span className="arrow right-arrow">&rarr;</span>
+          <span className="arrow right-arrow" onClick={() => {
+            if (currImageIndex + 2 < gallery.gallery.length - 1) {
+              setCurrImageIndex(currImageIndex + 1);
+            }
+          }}>&rarr;</span>
         </div>
       )}
     </div>
   );
 }
-/*
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- * 
- *
- *
- *
- * 
- *
- */
-
+//
+//
+//
+//
+//
+//
+//
+//
+//
