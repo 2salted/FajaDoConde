94257908a0268c4a07818c7293bfa5a4e45674bb
2salted <noamgraf123@gmail.com>
2024-09-20 00:03:51 +0000

    upadte

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import './App.css';
 import whatsapp from '/images/whatsapp.png';
 import galleryMedia from './galleryObjects.js'
+import { ChevronLeft, ChevronRight, X } from 'lucide-react';
 
 export default function App() {
   const [btnImage, setBtnImage] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [currImageIndex, setCurrImageIndex] = useState(0);
+  let arrowCss;
   let gallery;
 
   const searchGallery = (key) => {
@@ -57,29 +59,33 @@ export default function App() {
             overflow: 'hidden',
           }}
         >
-          <span onClick={() => setShowModal(false)} className="sm:right-20 absolute top-12 right-10 text-white text-4xl font-bold hover:text-red-500 hover:cursor-pointer focus:outline-none">&times;</span>
+          <span onClick={() => setShowModal(false)}
+            className="sm:right-20 absolute top-12 right-10 text-white text-4xl hover:text-red-500 
+            hover:cursor-pointer focus:outline-none"><X size={30}></X></span>
           <span className="arrow left-arrow" onClick={() => {
             if (currImageIndex > 0) {
               setCurrImageIndex(currImageIndex - 1);
             }
-          }}>&larr;</span>
+          }}><ChevronLeft size={40} /></span>
           <div className="slider">
-            <div>
-              <img src={gallery.gallery[currImageIndex]} width={100} height={100} />
-              <img src={gallery.gallery[currImageIndex + 1]} width={100} height={100} />
-              <img src={gallery.gallery[currImageIndex + 2]} width={100} height={100} />
-            </div>
+            <img src={gallery.gallery[currImageIndex]} width={100} height={100} className='' />
+            <img src={gallery.gallery[currImageIndex + 1]} width={100} height={100} className='w-1/2' />
+            <img src={gallery.gallery[currImageIndex + 2]} width={100} height={100} className='' />
           </div>
           <span className="arrow right-arrow" onClick={() => {
             if (currImageIndex + 2 < gallery.gallery.length - 1) {
               setCurrImageIndex(currImageIndex + 1);
             }
-          }}>&rarr;</span>
+          }}><ChevronRight size={40} /></span>
         </div>
       )}
     </div>
   );
 }
+//
+//
+//
+//
 //
 //
 //
